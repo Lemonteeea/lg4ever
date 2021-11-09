0effb37a1fc65416c94661428f810857aa2c751b
Lemonteeea <982813440@qq.com>
2021-11-09 13:31:03 +0800

    Updates

---
+++ docs/list.js
@@ -0,0 +1,16 @@
+const list = [
+  {
+    title: "001",
+    list: [
+      {
+        title: "06 | 一个网络请求是怎么进行的？",
+        datafile: "file01.js",
+      },
+      {
+        title: "",
+        datafile: "file02.js",
+      },
+    ],
+  },
+]
+export default list
