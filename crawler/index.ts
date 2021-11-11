ccb62788e4672345aa91afec9de2bbed4e4a8da5
Lemonteeea <982813440@qq.com>
2021-11-11 14:10:07 +0800

    使用json保存数据

--- crawler/index.ts
+++ crawler/index.ts
@@ -1,60 +1,62 @@
 import path from "path"
 import fs from "fs"
 const resList = [] as any[]
+// 写入文本到文件
 function writeDoc(filename: string, doc: string, cb?: any) {
   const file = path.resolve(__dirname, `../static/docs/${filename}`)
   const voidFunc = () => {}
   fs.writeFile(file, doc, cb || voidFunc)
 }
+// 从指定目录中获取文件内容
 function readHtml(id: string, filename: string) {
   const fileStream = fs.readFileSync(
     path.resolve(__dirname, `./rawHtml/${id}/${filename}`)
   )
   return fileStream.toString("utf8") as string
 }
+// 获取文档字符串
 function getDocStr(htmlStr: string) {
-  const reg = /<h1.*class=\"main-title\">.*\n\s*(.*)\n.*<\/h1>/
-  const title = reg.exec(htmlStr)![1]
   const startIndex = htmlStr.indexOf("rich-text-wrap") + 16
   htmlStr = htmlStr.substring(startIndex)
-  const endIndex = htmlStr.indexOf("</div>")
+  const endIndex = htmlStr.indexOf("el-dialog__wrapper")
   htmlStr = htmlStr.substring(0, endIndex)
-  const docStr = `
-  const article = {
-    title: '${title}',
-    context: \`${htmlStr}\`
-  } 
-  export default article
-`
-  return docStr
+  const lastIndex = htmlStr.lastIndexOf("</div>") - 50
+  htmlStr = htmlStr.substring(0, lastIndex).replace(/`/g, "\\`")
+  return htmlStr
 }
 
 // 生成专栏数据
 function createColumn(dirName: string) {
   const id = dirName
   const columnTitle = "前端进击笔记"
-  const list = [] as string[]
+  const list = [] as any[]
   const fileList = fs.readdirSync(path.resolve(__dirname, `./rawHtml/${id}`))
   fileList.forEach(filename => {
     let htmlStr = readHtml(id, filename).trim()
+    const reg = /<h1.*class=\"main-title\">.*\n\s*(.*)\n.*<\/h1>/
+    const title = reg.exec(htmlStr)![1]
     const docStr = getDocStr(htmlStr)
+    const destStr = JSON.stringify({
+      title,
+      context: docStr,
+    })
     const docId = parseInt(filename.replace(".html", ""))
-    const name = id + (Array(2).join("0") + docId).slice(-2) + ".js"
-    writeDoc(name, docStr)
-    list.push(name)
+    const name = id + (Array(2).join("0") + docId).slice(-2) + ".json"
+    writeDoc(name, destStr)
+    list.push({ title, name })
   })
-  resList.push(`{
-    title: "${id}", // ${columnTitle}
-    list: ${JSON.stringify(list)},
-  }`)
+  resList.push({
+    id,
+    title: columnTitle,
+    list: list,
+  })
 }
 function start() {
   const dirList = fs.readdirSync(path.resolve(__dirname, "./rawHtml"))
   dirList.forEach(dirName => {
     createColumn(dirName)
   })
-  const listStr = `const list = [${resList}]
-export default list`
-  writeDoc("list.ts", listStr)
+  const listStr = JSON.stringify(resList)
+  writeDoc("list.json", listStr)
 }
 start()
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,33 @@
+import axios from "axios"
 import React, { useEffect, useState } from "react"
-import list from "../../static/docs/list"
 import Article from "../components/Article"
 
 export default function Home() {
-  const column = list[0]
-  const doc = column.list[0]
   const [articleNode, setArticleNode] = useState(<div></div>)
   useEffect(() => {
-    import(`../../static/docs/${doc}`).then(doc => {
-      const article = doc.default as { title: string; context: string }
-      setArticleNode(
-        <Article title={article.title} context={article.context}></Article>
-      )
-    })
-  })
-  return <div className="page">{articleNode}</div>
+    axios
+      .get(`docs/list.json`)
+      .then(res => {
+        const list = res.data
+        console.log(list)
+        const column = list[0]
+        const doc = column.list[0]
+        console.log(doc)
+        return axios.get(`docs/${doc.name}`)
+      })
+      .then(doc => {
+        console.log(doc)
+        const article = doc.data as { title: string; context: string }
+        console.log(article)
+        setArticleNode(
+          <Article title={article.title} context={article.context}></Article>
+        )
+      })
+  }, [])
+  return (
+    <div className="page container">
+      <div className="container mx-auto h-5 border-b-2"></div>
+      {articleNode}
+    </div>
+  )
 }
