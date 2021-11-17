06696a600d66baf8dfdfc7faa6e1e6d9e3c3811e
Lemonteeea <982813440@qq.com>
2021-11-17 15:37:06 +0800

    适配手机屏幕

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import Article from "../components/Article"
 interface SectionData {
   title: string // 章节标题名
@@ -37,26 +37,38 @@ export default function Home() {
     })
   }, [])
   return (
-    <div className="container">
-      <div className="fixed left-0 right-0 top-0 bg-white z-10 h-16 border-b-2"></div>
-      <div className="page relative top-16 bottom-0 overflow-y-auto overflow-x-hidden shadow-lg">
-        <div className="fixed pt-10 text-opacity-80 text-sm w-80 h-full border-r-2">
-          <header className="font-semibold text-base p-6">章节目录</header>
-          <ul>
-            {sectionList.map((section, index) => {
-              return (
-                <li
-                  onClick={() => clickSection(index)}
-                  className="text-gray-800 mb-2 overflow-ellipsis border-b-2 p-2 cursor-pointer hover:text-blue-400"
-                >
-                  {section.title}
-                </li>
-              )
-            })}
-          </ul>
+    <div className="flex flex-col w-screen h-screen">
+      <div className="w-full h-16 bg-white border-b-2"></div>
+      <div className="flex flex-1 w-full justify-center overflow-hidden">
+        <div className="flex 2xl:w-1280 xl:w-1024 lg:w-950 w-full h-full shadow-lg">
+          <div className="lg:w-40 xl:w-60 2xl:w-80 w-0 h-full mt-16 overflow-hidden">
+            <header className="container font-semibold text-base p-6">
+              章节目录
+            </header>
+            <ul>
+              {sectionList.map((section, index) => {
+                return (
+                  <li
+                    onClick={() => clickSection(index)}
+                    className="text-gray-800 mb-2 overflow-ellipsis border-b-2 p-2 cursor-pointer hover:text-blue-400"
+                  >
+                    {section.title}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className="flex-1 h-full lg:p-20 p-5 pt-10 overflow-y-auto context">
+            {articleNode}
+          </div>
         </div>
-        <div className="ml-80 p-20 pt-10">{articleNode}</div>
       </div>
+      {/* <div className="page relative top-16 bottom-0 overflow-y-auto overflow-x-hidden ">
+        <div className="fixed pt-10 text-opacity-80 text-sm w-80 h-full border-r-2 sm:hidden">
+          
+        </div>
+        <div className="container xl:ml-80 "></div>
+      </div> */}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        1280: "1280px",
+        1024: "1024px",
+        950: "950px",
+      },
+    },
   },
   variants: {
     extend: {},
